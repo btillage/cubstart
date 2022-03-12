6111eef1cbcd5a4ec7db0f5ed0bba863ea522bd7
btillage <82730760+btillage@users.noreply.github.com>
2022-03-11 18:49:25 -0800

    Add files via upload

---
+++ hw4/waitlisthelper-skeleton/waitlisthelper_skeletonApp.swift
@@ -0,0 +1,19 @@
+//
+//  waitlisthelper_skeletonApp.swift
+//  waitlisthelper-skeleton
+//
+//  Created by Jordan Yee on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct waitlisthelper_skeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+            
+        }
+    }
+}
